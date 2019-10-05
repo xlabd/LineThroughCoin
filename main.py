d6453b8fe2401d687273254cb4418853d10ecc86
Labhesh Valechha <labheshvalechha@Labheshs-MacBook-Air.local>
2019-10-05 17:35:34 +0530

    Removed small holes and found centroids

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np 
 import cv2 
+from skimage import morphology
 
 img = cv2.imread('coin.jpg', 0)         #read
 #cv2.imshow('image', img)                #show
@@ -16,16 +17,35 @@ if cv2.waitKey(0) & 0xff == 27:
 '''closing'''
 kernel = np.ones((3, 3), np.uint8)
 closing = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=4)                                       #morphology operation closing
-    
+
 '''fill holes'''
 im_floodfill = closing.copy()           # Copy the thresholded image.
 # Mask used to flood filling. Notice the size needs to be 2 pixels than the image.
 h, w = closing.shape[:2]
-mask = np.zeros((h+2, w+2), np.uint8)
- 
+mask = np.zeros((h+2, w+2), np.uint8) 
 cv2.floodFill(im_floodfill, mask, (0,0), 255);               # Floodfill from point (0, 0)
 im_floodfill_inv = cv2.bitwise_not(im_floodfill)        # Invert floodfilled image
 im_out = closing | im_floodfill_inv                             # Combine the two images to get the foreground.
 cv2.imshow("Foreground", im_out)                            # Display images.
 if cv2.waitKey(0) & 0xff == 27:  
     cv2.destroyAllWindows()             # De-allocate any associated memory usage
+
+'''destroy small holes'''
+im_out = cv2.erode(im_out, None, iterations=8)
+im_out = cv2.dilate(im_out, None, iterations=8)
+cv2.imshow("Holes removed", im_out)                            # Display images.
+if cv2.waitKey(0) & 0xff == 27:  
+    cv2.destroyAllWindows()             # De-allocate any associated memory usage    
+'''centroid'''
+# find contours in the binary image
+contours, hierarchy = cv2.findContours(im_out,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+for c in contours:
+   # calculate moments for each contour
+    M = cv2.moments(c)
+    # calculate x,y coordinate of center
+    cX = int(M["m10"] / M["m00"])
+    cY = int(M["m01"] / M["m00"])
+    cv2.circle(img, (cX, cY), 5, (255, 255, 255), -1)
+    cv2.putText(img, "centroid", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+    cv2.imshow("Image", img)
+    cv2.waitKey(0)
